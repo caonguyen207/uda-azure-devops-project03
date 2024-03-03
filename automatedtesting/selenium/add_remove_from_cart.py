d00a8be3fe0596ae03ada59e39d3548c18e7185b
Nguyen, Le <caonguyenle007@gmail.com>
2024-03-03 20:22:35 +0700

    update selenium test

--- automatedtesting/selenium/add_remove_from_cart.py
+++ automatedtesting/selenium/add_remove_from_cart.py
@@ -1,14 +1,11 @@
 # #!/usr/bin/env python
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService 
 from selenium.webdriver.common.by import By
 
-print('Starting the browser...')
-options = Options()	
-options.add_argument("--headless") 
-driver = webdriver.Chrome(options=options)
-print('Browser started successfully. Navigating to the demo page to login.')
-
+options = webdriver.ChromeOptions()
+options.headless = True 
 
 def login(user, password):
   driver.get('https://www.saucedemo.com/')
@@ -28,12 +25,14 @@ def add_item ():
   driver.find_element(By.ID,'add-to-cart-sauce-labs-backpack').click()
   no_of_items = driver.find_element(By.CSS_SELECTOR, "span.shopping_cart_badge").text
   return no_of_items == '1'
+  print ('Add single item completed!')
 
 def remove_item ():
   driver.get("https://www.saucedemo.com/inventory.html")
   driver.find_element(By.ID,'remove-sauce-labs-backpack').click()
   items = driver.find_elements(By.CLASS_NAME, "cart_item")
   return len(items) == 0
+  print ('Remove single item completed!')
 
 def add_all_items():
   driver.get("https://www.saucedemo.com/inventory.html")
@@ -42,6 +41,7 @@ def add_all_items():
     item.find_element(By.CSS_SELECTOR, "button.btn_inventory").click()
   no_of_items = driver.find_element(By.CSS_SELECTOR, "span.shopping_cart_badge").text
   assert no_of_items == '6'
+  print ('Add all items completed!')
 
 def remove_all_items():
   driver.get("https://www.saucedemo.com/cart.html")
@@ -50,14 +50,20 @@ def remove_all_items():
     item.find_element(By.CLASS_NAME, "cart_button").click()
   items = driver.find_elements(By.CLASS_NAME, "cart_item")
   return len(items) == 0
+  print ('Remove all items completed!')
 
-try:
-  login('standard_user', 'secret_sauce')
+print('Starting the browser...')
+with webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options) as driver:
+# driver = webdriver.Chrome(ChromeDriverManager().install(), )
+    print('Browser started successfully. Navigating to the demo page to login.')
+    try:
+        login('standard_user', 'secret_sauce')
 
-  add_item()
-  remove_item()
-  add_all_items()
-  remove_all_items()
-except:
-  # Close the driver
-  driver.close()
+        add_item()
+        remove_item()
+        add_all_items()
+        remove_all_items()
+        print('Completed all test case')
+    except:
+        # Close the driver
+        driver.close()
