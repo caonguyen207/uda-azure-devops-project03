d61097af345ea7eaf24f325d09c45526b46bbc51
Nguyen, Le <caonguyenle007@gmail.com>
2024-03-05 16:39:18 +0700

    add logging

--- automatedtesting/selenium/add_remove_from_cart.py
+++ automatedtesting/selenium/add_remove_from_cart.py
@@ -3,6 +3,12 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService 
 from selenium.webdriver.common.by import By
+import logging
+
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
 
 options = webdriver.ChromeOptions()
 options.headless = True
@@ -13,11 +19,11 @@ def login(user, password):
   driver.find_element(By.CSS_SELECTOR, "input[id='password']").send_keys(password)
   driver.find_element(By.CSS_SELECTOR, "input[id='login-button']").click()
   if driver.current_url == 'https://www.saucedemo.com/inventory.html':
-    print('Successfully logged in with user: ' + user)
+    logging.info ('Successfully logged in with user: ' + user)
     logo = driver.find_element(By.CSS_SELECTOR, ".header_label>.app_logo").text
     return "${logo}" == "Swag Labs"
   else:
-    print ('Failed to logged in')
+    logging.info  ('Failed to logged in')
     return False
 
 def add_item ():
@@ -25,14 +31,14 @@ def add_item ():
   driver.find_element(By.ID,'add-to-cart-sauce-labs-backpack').click()
   no_of_items = driver.find_element(By.CSS_SELECTOR, "span.shopping_cart_badge").text
   return no_of_items == '1'
-  print ('Add single item completed!')
+  logging.info  ('Add single item completed!')
 
 def remove_item ():
   driver.get("https://www.saucedemo.com/inventory.html")
   driver.find_element(By.ID,'remove-sauce-labs-backpack').click()
   items = driver.find_elements(By.CLASS_NAME, "cart_item")
   return len(items) == 0
-  print ('Remove single item completed!')
+  logging.info  ('Remove single item completed!')
 
 def add_all_items():
   driver.get("https://www.saucedemo.com/inventory.html")
@@ -41,7 +47,7 @@ def add_all_items():
     item.find_element(By.CSS_SELECTOR, "button.btn_inventory").click()
   no_of_items = driver.find_element(By.CSS_SELECTOR, "span.shopping_cart_badge").text
   assert no_of_items == '6'
-  print ('Add all items completed!')
+  logging.info  ('Add all items completed!')
 
 def remove_all_items():
   driver.get("https://www.saucedemo.com/cart.html")
@@ -50,15 +56,15 @@ def remove_all_items():
     item.find_element(By.CLASS_NAME, "cart_button").click()
   items = driver.find_elements(By.CLASS_NAME, "cart_item")
   return len(items) == 0
-  print ('Remove all items completed!')
+  logging.info  ('Remove all items completed!')
 
-print('Starting the browser...')
+logging.info ('Starting the browser...')
 # Chrome version and chromedriver must be same
 # https://stackoverflow.com/questions/60296873/sessionnotcreatedexception-message-session-not-created-this-version-of-chrome
 # https://sites.google.com/chromium.org/driver/downloads/version-selection
 # driver = webdriver.Chrome(executable_path='./chromedriver.exe', options=options)
 driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver', options=options)
-print('Browser started successfully. Navigating to the demo page to login.')
+logging.info ('Browser started successfully. Navigating to the demo page to login.')
 try:
     login('standard_user', 'secret_sauce')
 
@@ -66,7 +72,7 @@ try:
     remove_item()
     add_all_items()
     remove_all_items()
-    print('Completed all test case')
+    logging.info ('Completed all test case')
 except:
     # Close the driver
     driver.close()
